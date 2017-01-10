ce6bf5253719572921e96e4807a8bf4995c718b4
1Computer1 <mineox100@gmail.com>
2017-01-09 19:39:07 -0500

    Make allowMention also take a function.

--- src/struct/Framework.js
+++ src/struct/Framework.js
@@ -7,8 +7,8 @@ const ListenerHandler = require('./events/ListenerHandler');
  * @prop {string} token - Client token.
  * @prop {string} ownerID - Discord ID of the client owner.
  * @prop {boolean} selfbot - Marks this bot as a selfbot.
- * @prop {string} prefix - Default command prefix or function returning prefix.
- * @prop {boolean} allowMention - Allows mentions to the client user as a prefix.
+ * @prop {(string|function)} prefix - Default command prefix or function returning prefix.
+ * @prop {(boolean|function)} allowMention - Allow mentions to the client user as a prefix or function that returns true or false.
  * @prop {string} commandDirectory - Directory to commands.
  * @prop {string} inhibitorDirectory - Directory to inhibitors.
  * @prop {string} listenerDirectory - Directory to listeners.
@@ -64,6 +64,7 @@ class Framework {
 
             this.client.on('message', message => {
                 let prefix;
+                let allowMention;
 
                 if (typeof this.options.prefix === 'function'){
                     prefix = this.options.prefix(message) || '!';
@@ -71,7 +72,13 @@ class Framework {
                     prefix = this.options.prefix || '!';
                 }
 
-                this.commandHandler.handle(message, prefix, this.options.allowMention);
+                if (typeof this.options.allowMention === 'function'){
+                    allowMention = this.options.allowMention(message) || false;
+                } else {
+                    allowMention = this.options.allowMention || false;
+                }
+
+                this.commandHandler.handle(message, prefix, allowMention);
             });
         });
     }
