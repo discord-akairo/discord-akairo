f8e2f84daae9e3ef2de34fcfee3dac4051c1f838
1Computer1 <mineox100@gmail.com>
2017-01-21 02:33:18 -0500

    Use perm flags from the lib.

--- src/struct/utils/ClientUtil.js
+++ src/struct/utils/ClientUtil.js
@@ -1,35 +1,4 @@
-const PERMISSIONS = {
-    CREATE_INSTANT_INVITE: 1 << 0,
-    KICK_MEMBERS: 1 << 1,
-    BAN_MEMBERS: 1 << 2,
-    ADMINISTRATOR: 1 << 3,
-    MANAGE_CHANNELS: 1 << 4,
-    MANAGE_GUILD: 1 << 5,
-    ADD_REACTIONS: 1 << 6,
-
-    READ_MESSAGES: 1 << 10,
-    SEND_MESSAGES: 1 << 11,
-    SEND_TTS_MESSAGES: 1 << 12,
-    MANAGE_MESSAGES: 1 << 13,
-    EMBED_LINKS: 1 << 14,
-    ATTACH_FILES: 1 << 15,
-    READ_MESSAGE_HISTORY: 1 << 16,
-    MENTION_EVERYONE: 1 << 17,
-    EXTERNAL_EMOJIS: 1 << 18,
-
-    CONNECT: 1 << 20,
-    SPEAK: 1 << 21,
-    MUTE_MEMBERS: 1 << 22,
-    DEAFEN_MEMBERS: 1 << 23,
-    MOVE_MEMBERS: 1 << 24,
-    USE_VAD: 1 << 25,
-
-    CHANGE_NICKNAME: 1 << 26,
-    MANAGE_NICKNAMES: 1 << 27,
-    MANAGE_ROLES_OR_PERMISSIONS: 1 << 28,
-    MANAGE_WEBHOOKS: 1 << 29,
-    MANAGE_EMOJIS: 1 << 30,
-};
+const PermissionFlags = require('discord.js').Constants.PermissionFlags;
 
 class ClientUtil {
     /**
@@ -288,8 +257,8 @@ class ClientUtil {
     resolvePermissionNumber(number){
         let resolved = [];
 
-        Object.keys(PERMISSIONS).forEach(key => {
-            if (number & PERMISSIONS[key]) resolved.push(key);
+        Object.keys(PermissionFlags).forEach(key => {
+            if (number & PermissionFlags[key]) resolved.push(key);
         });
 
         return resolved;
