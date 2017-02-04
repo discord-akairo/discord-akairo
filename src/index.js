0c44b4e59b94072910d5b42d555f104b995dcd57
1Computer1 <mineox100@gmail.com>
2017-02-03 23:39:03 -0500

    Almost forgot.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ module.exports = {
     Command: require('./struct/commands/Command'),
     Inhibitor: require('./struct/inhibitors/Inhibitor'),
     Listener: require('./struct/listeners/Listener'),
-    DatabaseHandler: require('./struct/databases/DatabaseHandler'),
     SQLiteHandler: require('./struct/databases/SQLiteHandler'),
     Constants: require('./struct/utils/Constants'),
     version: require('../package.json').version
