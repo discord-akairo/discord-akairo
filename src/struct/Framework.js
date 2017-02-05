b236fc345abb44a8cd7c66b93af97c38adb97ae1
1Computer1 <mineox100@gmail.com>
2017-02-05 00:16:00 -0500

    Docs.

--- src/struct/Framework.js
+++ src/struct/Framework.js
@@ -15,7 +15,7 @@ const ClientUtil = require('./utils/ClientUtil');
  * @prop {boolean} [preInhibitors=false] - Disables the built-in pre-message inhibitors.
  * @prop {boolean} [postInhibitors=false] - Disables the built-in post-message inhibitors.
  * @prop {string} [listenerDirectory] - Directory to listeners.
- * @prop {Object} [emitters] - Emitters to load onto the listener handler.
+ * @prop {Object} [emitters={}] - Emitters to load onto the listener handler.
  */
 
 class Framework {
