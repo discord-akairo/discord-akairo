22578eafa899101c4e3af464886dc35052dbbed7
1Computer1 <mineox100@gmail.com>
2017-03-07 01:47:44 -0500

    Expose the new things

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 module.exports = {
     AkairoClient: require('./struct/AkairoClient'),
     AkairoHandler: require('./struct/AkairoHandler'),
-    CommandHandler: require('./struct/CommandHandler'),
-    InhibitorHandler: require('./struct/InhibitorHandler'),
-    ListenerHandler: require('./struct/ListenerHandler'),
     AkairoModule: require('./struct/AkairoModule'),
+    Argument: require('./struct/Argument'),
     Command: require('./struct/Command'),
+    CommandHandler: require('./struct/CommandHandler'),
     Inhibitor: require('./struct/Inhibitor'),
+    InhibitorHandler: require('./struct/InhibitorHandler'),
     Listener: require('./struct/Listener'),
-    SQLiteHandler: require('./util/SQLiteHandler'),
+    ListenerHandler: require('./struct/ListenerHandler'),
+    Category: require('./util/Category'),
     ClientUtil: require('./util/ClientUtil'),
     Constants: require('./util/Constants'),
-    Category: require('./util/Category'),
+    SQLiteHandler: require('./util/SQLiteHandler'),
+    TypeResolver: require('./util/TypeResolver'),
     version: require('../package.json').version
 };
