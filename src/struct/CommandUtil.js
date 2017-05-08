5ff1d620710742229a9112d7cd2c8fc4ac09b84b
1Computer1 <mineox100@gmail.com>
2017-05-08 18:40:13 -0400

    Fix for files

--- src/struct/CommandUtil.js
+++ src/struct/CommandUtil.js
@@ -71,10 +71,7 @@ class CommandUtil {
      * @returns {void}
      */
     setLastResponse(message) {
-        if (message.command) {
-            if (!this.command.handler.handleEdits || !this.command.editable) return;
-        }
-
+        if (message.command && (!this.command.handler.handleEdits || !this.command.editable)) return;
         if (Array.isArray(message)) {
             this.lastResponse = message.slice(-1)[0];
         } else {
@@ -91,12 +88,12 @@ class CommandUtil {
     send(content, options) {
         [content, options] = this.constructor.swapOptions(content, options);
 
-        if (this.shouldEdit && (this.command ? this.command.editable : true) && (!options.file || this.lastResponse.attachments.size)) {
+        if (this.shouldEdit && (this.command ? this.command.editable : true) && (!(options.files || options.file) || this.lastResponse.attachments.size)) {
             return this.lastResponse.edit(content, options);
         }
 
         return this.message.channel.send(content, options).then(sent => {
-            if (options.file) return sent;
+            if (options.files || options.file) return sent;
             if (this.lastResponse && this.lastResponse.attachments.size) return sent;
 
             this.shouldEdit = true;
