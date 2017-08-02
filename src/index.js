ddce824fdcad8793bc52ae7a1a6c99364af63fcc
1Computer1 <mineox100@gmail.com>
2017-08-02 19:00:53 -0400

    Organize index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 module.exports = {
+    // Core
     AkairoClient: require('./struct/AkairoClient'),
     AkairoHandler: require('./struct/AkairoHandler'),
     AkairoModule: require('./struct/AkairoModule'),
+
+    // Commands
     Argument: require('./struct/Argument'),
-    ClientUtil: require('./struct/ClientUtil'),
     Command: require('./struct/Command'),
     CommandHandler: require('./struct/CommandHandler'),
     CommandUtil: require('./struct/CommandUtil'),
+    TypeResolver: require('./struct/TypeResolver'),
+
+    // Inhibitors
     Inhibitor: require('./struct/Inhibitor'),
     InhibitorHandler: require('./struct/InhibitorHandler'),
+
+    // Listeners
     Listener: require('./struct/Listener'),
     ListenerHandler: require('./struct/ListenerHandler'),
+
+    // Providers
+    Provider: require('./providers/Provider'),
+    SequelizeProvider: require('./providers/SequelizeProvider'),
     SQLiteHandler: require('./struct/SQLiteHandler'),
-    TypeResolver: require('./struct/TypeResolver'),
+    SQLiteProvider: require('./providers/SQLiteProvider'),
+
+    // Utilities
     Category: require('./util/Category'),
+    ClientUtil: require('./struct/ClientUtil'),
     Constants: require('./util/Constants'),
     version: require('../package.json').version
 };
