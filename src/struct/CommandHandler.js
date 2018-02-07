e067c7fadd568ece78c7f8ac90045ac1da154c12
1Computer1 <mineox100@gmail.com>
2018-02-06 23:41:25 -0500

    Fix for Message#member now being a getter

--- src/struct/CommandHandler.js
+++ src/struct/CommandHandler.js
@@ -309,7 +309,7 @@ class CommandHandler extends AkairoHandler {
     async handle(message) {
         try {
             if (this.fetchMembers && message.guild && !message.member && !message.webhookID) {
-                message.member = await message.guild.members.fetch(message.author);
+                await message.guild.members.fetch(message.author);
             }
 
             let reason = this.client.inhibitorHandler
