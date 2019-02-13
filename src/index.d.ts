43813df66ffcd4f785a9252a5665800dd22afb8a
Reseq64 <47262222+Reseq64@users.noreply.github.com>
2019-02-13 22:10:57 +0100

    Update typings
    typings were not working in case of using `prompt: true` when using `defaultPrompt` in the CommandHandler

--- src/index.d.ts
+++ src/index.d.ts
@@ -258,10 +258,8 @@ declare module 'discord-akairo' {
         public on(event: 'remove', listener: (command: Command) => any): this;
         public on(event: 'load', listener: (command: Command, isReload: boolean) => any): this;
         public on(event: 'commandBlocked', listener: (message: Message, command: Command, reason: string) => any): this;
-        public on(event: 'commandBreakout', listener: (message: Message, command: Command, breakMessage: Message) => any): this;
         public on(event: 'commandCancelled', listener: (message: Message, command: Command, retryMessage?: Message) => any): this;
         public on(event: 'commandFinished', listener: (message: Message, command: Command, args: any, returnValue: any) => any): this;
-        public on(event: 'commandLocked', listener: (message: Message, command: Command) => any): this;
         public on(event: 'commandStarted', listener: (message: Message, command: Command, args: any) => any): this;
         public on(event: 'cooldown', listener: (message: Message, command: Command, remaining: number) => any): this;
         public on(event: 'error', listener: (error: Error, message: Message, command: Command) => any): this;
@@ -550,7 +548,7 @@ declare module 'discord-akairo' {
         match?: ArgumentMatch;
         multipleFlags?: boolean;
         flag?: string | string[];
-        prompt?: ArgumentPromptOptions;
+        prompt?: ArgumentPromptOptions | DefaultArgumentPromptOptions;
         type?: ArgumentType | ArgumentTypeCaster;
         unordered?: boolean | number | number[];
     };
@@ -582,6 +580,8 @@ declare module 'discord-akairo' {
         time?: number;
         timeout?: StringResolvable | MessageOptions | MessageAdditions | PromptContentSupplier;
     };
+
+    export type DefaultArgumentPromptOptions = boolean;
 
     export type CommandOptions = {
         aliases?: string[];
@@ -665,7 +665,7 @@ declare module 'discord-akairo' {
         | 'channel' | 'channels' | 'textChannel' | 'textChannels' | 'voiceChannel' | 'voiceChannels'
         | 'role' | 'roles' | 'emoji' | 'emojis' | 'guild' | 'guilds'
         | 'message' | 'guildMessage' | 'relevantMessage' | 'invite'
-        | 'userMention' | 'memberMention' | 'channelMention' | 'roleMention' | 'emojiMention'
+        | 'memberMention' | 'channelMention' | 'roleMention' | 'emojiMention'
         | 'commandAlias' | 'command' | 'inhibitor' | 'listener'
         | (string | string[])[]
         | RegExp
