981166d92428c28f05beb5940dc51feefd44300c
Gryffon Bellish <39341355+PyroTechniac@users.noreply.github.com>
2019-05-05 21:34:06 -0400

    bring checkGuild inline with the rest of the checks
    The rest of the check functions default caseSensitive and wholeWord to false, except checkGuild, this aims to fix that

--- src/struct/ClientUtil.js
+++ src/struct/ClientUtil.js
@@ -310,7 +310,7 @@ class ClientUtil {
      * @param {boolean} [wholeWord=false] - Makes checking by name match full word only.
      * @returns {boolean}
      */
-    checkGuild(text, guild, caseSensitive, wholeWord) {
+    checkGuild(text, guild, caseSensitive = false, wholeWord = false) {
         if (guild.id === text) return true;
 
         text = caseSensitive ? text : text.toLowerCase();
