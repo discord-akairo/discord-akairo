c6faf826dcfd2fabe3f2fc0dbe9c88b61bcbac3f
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2020-03-09 00:44:54 +0200

    Update trailing space

--- src/index.d.ts
+++ src/index.d.ts
@@ -25,7 +25,7 @@ declare module 'discord-akairo' {
         public constructor(options?: AkairoOptions & ClientOptions, clientOptions?: ClientOptions);
 
         public ownerID: Snowflake | Snowflake[];
-        
+
         public isOwner(user: UserResolvable): boolean;
     }
 
