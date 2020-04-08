a801b752f5f82e360d734e7d397fa987014fa16b
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2020-04-08 11:30:09 +0300

    tests: fix lock command

--- test/commands/lock.js
+++ test/commands/lock.js
@@ -12,7 +12,7 @@ class LockCommand extends Command {
     }
 
     exec(message) {
-        return [0, 1, 2, 3, 4].reduce((promise, num) => promise.then(() => sleep(1000)).then(() => message.util.send(num)), Promise.resolve());
+        return [0, 1, 2, 3, 4].reduce((promise, num) => promise.then(() => sleep(1000)).then(() => message.util.send(num.toString())), Promise.resolve());
     }
 }
 
