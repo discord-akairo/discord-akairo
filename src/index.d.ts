b0dfbff7da728828a2a4b0a5d70f61a6f6d59606
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2020-05-08 18:30:41 +0300

    revert

--- src/index.d.ts
+++ src/index.d.ts
@@ -686,32 +686,89 @@ declare module 'discord-akairo' {
     export type RegexSupplier = (message: Message) => RegExp;
 
     export const Constants: {
-        ArgumentMatches: Record<
-            'PHRASE' | 'FLAG' | 'OPTION' | 'REST' | 'SEPARATE' |
-            'TEXT' | 'CONTENT' | 'REST_CONTENT' | 'NONE', string
-        >;
-        ArgumentTypes: Record<
-            'STRING' | 'LOWERCASE' | 'UPPERCASE' | 'CHAR_CODES' |
-            'NUMBER' | 'INTEGER' | 'BIGINT' | 'EMOJINT' |
-            'URL' | 'DATE' | 'COLOR' | 'USER' |
-            'USERS' | 'MEMBER' | 'MEMBERS' | 'RELEVANT' | 'RELEVANTS' |
-            'CHANNEL' | 'CHANNELS' | 'TEXT_CHANNEL' | 'TEXT_CHANNELS' |
-            'VOICE_CHANNEL' | 'VOICE_CHANNELS' | 'CATEGORY_CHANNEL' | 'CATEGORY_CHANNELS' |
-            'NEWS_CHANNEL' | 'NEWS_CHANNELS' | 'STORE_CHANNEL' | 'STORE_CHANNELS' |
-            'ROLE' | 'ROLES' | 'EMOJI' | 'EMOJIS' | 'GUILD' | 'GUILDS' |
-            'MESSAGE' | 'GUILD_MESSAGE' | 'RELEVANT_MESSAGE' | 'INVITE' |
-            'MEMBER_MENTION' | 'CHANNEL_MENTION' | 'ROLE_MENTION' | 'EMOJI_MENTION' |
-            'COMMAND_ALIAS' | 'COMMAND' | 'INHIBITOR' | 'LISTENER', string
-        >;
-        AkairoHandlerEvents: Record<'LOAD' | 'REMOVE', string>;
-        CommandHandlerEvents: Record<
-            'MESSAGE_BLOCKED' | 'MESSAGE_INVALID' |
-            'COMMAND_BLOCKED' | 'COMMAND_STARTED' |
-            'COMMAND_FINISHED' | 'COMMAND_CANCELLED' |
-            'COMMAND_LOCKED' | 'MISSING_PERMISSIONS' |
-            'COOLDOWN' | 'IN_PROMPT' | 'ERROR', string
-        >;
-        BuiltInReasons: Record<'CLIENT' | 'BOT' | 'OWNER' | 'GUILD' | 'DM', string>;
+        ArgumentMatches: {
+            PHRASE: 'phrase';
+            FLAG: 'flag';
+            OPTION: 'option';
+            REST: 'rest';
+            SEPARATE: 'separate';
+            TEXT: 'text';
+            CONTENT: 'content';
+            REST_CONTENT: 'restContent';
+            NONE: 'none';
+        };
+        ArgumentTypes: {
+            STRING: 'string';
+            LOWERCASE: 'lowercase';
+            UPPERCASE: 'uppercase';
+            CHAR_CODES: 'charCodes';
+            NUMBER: 'number';
+            INTEGER: 'integer';
+            BIGINT: 'bigint';
+            EMOJINT: 'emojint';
+            URL: 'url';
+            DATE: 'date';
+            COLOR: 'color';
+            USER: 'user';
+            USERS: 'users';
+            MEMBER: 'member';
+            MEMBERS: 'members';
+            RELEVANT: 'relevant';
+            RELEVANTS: 'relevants';
+            CHANNEL: 'channel';
+            CHANNELS: 'channels';
+            TEXT_CHANNEL: 'textChannel';
+            TEXT_CHANNELS: 'textChannels';
+            VOICE_CHANNEL: 'voiceChannel';
+            VOICE_CHANNELS: 'voiceChannels';
+            CATEGORY_CHANNEL: 'categoryChannel';
+            CATEGORY_CHANNELS: 'categoryChannels';
+            NEWS_CHANNEL: 'newsChannel';
+            NEWS_CHANNELS: 'newsChannels';
+            STORE_CHANNEL: 'storeChannel';
+            STORE_CHANNELS: 'storeChannels';
+            ROLE: 'role';
+            ROLES: 'roles';
+            EMOJI: 'emoji';
+            EMOJIS: 'emojis';
+            GUILD: 'guild';
+            GUILDS: 'guilds';
+            MESSAGE: 'message';
+            GUILD_MESSAGE: 'guildMessage';
+            INVITE: 'invite';
+            MEMBER_MENTION: 'memberMention';
+            CHANNEL_MENTION: 'channelMention';
+            ROLE_MENTION: 'roleMention';
+            EMOJI_MENTION: 'emojiMention';
+            COMMAND_ALIAS: 'commandAlias';
+            COMMAND: 'command';
+            INHIBITOR: 'inhibitor';
+            LISTENER: 'listener';
+        };
+        AkairoHandlerEvents: {
+            LOAD: 'load';
+            REMOVE: 'remove';
+        };
+        CommandHandlerEvents: {
+            MESSAGE_BLOCKED: 'messageBlocked';
+            MESSAGE_INVALID: 'messageInvalid';
+            COMMAND_BLOCKED: 'commandBlocked';
+            COMMAND_STARTED: 'commandStarted';
+            COMMAND_FINISHED: 'commandFinished';
+            COMMAND_CANCELLED: 'commandCancelled';
+            COMMAND_LOCKED: 'commandLocked';
+            MISSING_PERMISSIONS: 'missingPermissions';
+            COOLDOWN: 'cooldown';
+            IN_PROMPT: 'inPrompt';
+            ERROR: 'error';
+        };
+        BuiltInReasons: {
+            CLIENT: 'client';
+            BOT: 'bot';
+            OWNER: 'owner';
+            GUILD: 'guild';
+            DM: 'dm';
+        };
     };
 
     export const version: string;
