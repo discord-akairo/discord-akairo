1159a745a6c4a0352a6c3db933e1410583a3efe7
Erica DiGiulio <digiulioem@gmail.com>
2020-07-09 00:42:00 -0600

    Add constant definition for new 'required' default type

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -11,6 +11,7 @@ module.exports = {
         NONE: 'none'
     },
     ArgumentTypes: {
+        REQUIRED: 'required',
         STRING: 'string',
         LOWERCASE: 'lowercase',
         UPPERCASE: 'uppercase',
