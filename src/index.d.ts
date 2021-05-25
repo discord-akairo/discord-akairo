dd86bdb49de5d798fc6e184bd8c5733ec89a2270
Dhruvin-Purohit <73269605+Dhruvin-Purohit@users.noreply.github.com>
2021-05-25 18:11:00 +0530

    Merge Commit Suggestion from almedix
    Co-authored-by: Almeida <almeidx@pm.me>

--- src/index.d.ts
+++ src/index.d.ts
@@ -133,7 +133,7 @@ declare module 'discord-akairo' {
         public compareStreaming(oldMember: GuildMember, newMember: GuildMember): number;
         public embed(data?: object): MessageEmbed;
         public fetchMember(guild: Guild, id: string, cache?: boolean): Promise<GuildMember>;
-        public permissionNames(): string[];
+        public permissionNames(): (keyof Permissions['FLAGS'])[];
         public resolveChannel(text: string, channels: Collection<Snowflake, Channel>, caseSensitive?: boolean, wholeWord?: boolean): Channel;
         public resolveChannels(text: string, channels: Collection<Snowflake, Channel>, caseSensitive?: boolean, wholeWord?: boolean): Collection<Snowflake, Channel>;
         public resolveEmoji(text: string, emojis: Collection<Snowflake, Emoji>, caseSensitive?: boolean, wholeWord?: boolean): Emoji;
